d9c6990294d55b24d2e9b59b39ed1819b987350f
Hudson Machado <41968106+huddmax@users.noreply.github.com>
2024-08-16 15:12:46 -0300

    Update api.js
    a Branch main vai lidar com a API local e a Branch de Deploy que vai lidar com a API ON

--- Front/src/services/api.js
+++ Front/src/services/api.js
@@ -2,6 +2,6 @@ import axios from "axios";
 
 export const api = axios.create({
     // Conexão do Front com a API
-    baseURL: "https://notes-api-c7xl.onrender.com"
-    // baseURL: "http://localhost:3333"
+    //baseURL: "https://notes-api-c7xl.onrender.com"
+    baseURL: "http://localhost:3333"
 })
