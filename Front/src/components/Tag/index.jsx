bdfddadced091748439852a5618f02f73e39d332
Hudson Machado <41968106+huddmax@users.noreply.github.com>
2024-08-16 17:27:48 -0300

    Delete Front directory

--- Front/src/components/Tag/index.jsx
+++
@@ -1,9 +0,0 @@
-import { Container } from './style.js';
-
-export function Tag({ title, ...rest }){
-    return (
-        <Container {...rest}>
-            {title}
-        </Container>
-    );
-} 
