ee01251afeb4f59a9fcd62c8155e453bf90cbb7d
Hudson Machado <41968106+huddmax@users.noreply.github.com>
2024-08-16 17:26:20 -0300

    Delete API directory

--- API/src/routes/sessions.routes.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require("express");
-
-const SessionController = require("../controllers/SessionsController");
-const sessionsController = new SessionController();
-
-const sessionsRoutes = Router();
-sessionsRoutes.post("/", sessionsController.create);
-
-module.exports = sessionsRoutes;
